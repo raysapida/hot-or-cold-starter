827dfa4e9dcc1b2e8f530bdd2eefaa1eb6be7410
Raymond Sapida <ray.sapida@gmail.com>
2015-05-27 00:35:30 -0700

    Add a newGame function and click handlers for the new game and guess buttons

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  var secretNumber
+  newGame();
 
-  	});
+  /*--- Display information modal box ---*/
+  $(".what").click(function(){
+    $(".overlay").fadeIn(1000);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  });
 
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function(){
+    $(".overlay").fadeOut(1000);
+  });
+
+
+  $(".clearfix").on('click', '.new', function() {
+    newGame();
+  });
+
+  $( "#guess" ).submit(function( event ) {
+    var input = +$("input").val();
+    alert(input);
+    event.preventDefault();
+  });
 });
 
+function newGame() {
+  secretNumber = Math.floor(Math.random() * 100) + 1
+  alert(secretNumber);
+}
 
